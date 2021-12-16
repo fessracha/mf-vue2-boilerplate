b781c5139c51f23e170ce9a023cf21fa54dea4dc
Eugene Shangin <fessracha@gmail.com>
2021-12-16 23:25:13 +0500

    Example error handler for loading mf

--- vue2-mf-reciever/src/components/PageDemo.vue
+++ vue2-mf-reciever/src/components/PageDemo.vue
@@ -2,14 +2,30 @@
   <div>
     <h3>Page for testing inject micro-frontend</h3>
     <!--MF wrapper-->
-    <div id="mf-counter"></div>
+    <div id="mf-scope"></div>
+    <div v-if="mfStatus === 'loading'">loading microfrontend...</div>
+    <div v-if="mfStatus === 'error'">microfrontend is not available :(</div>
   </div>
 </template>
 <script>
-import { initCounterApp } from 'counter_app/initCounterApp';
 export default {
+  data() {
+    return {
+      mfStatus: 'init'
+    }
+  },
   mounted() {
-    initCounterApp('#mf-counter');
+    this.mfStatus = 'loading';
+
+    import('counter_app/initCounterApp')
+        .then(({ initCounterApp }) => {
+          initCounterApp('#mf-scope')
+          this.mfStatus = 'success';
+        })
+        .catch(e => {
+          console.log('Error load MF');
+          this.mfStatus = 'error';
+        });
   }
 }
 </script>
